e212a429c13e4ce7a6536372ac7b945a5a641b41
Leandro Rizzo <86168414+LeandroRizzo@users.noreply.github.com>
2021-07-20 00:48:04 -0300

    Update app.js

--- app.js
+++ app.js
@@ -30,61 +30,51 @@ function guardarProducto(){
     
 
     let paso1 = JSON.parse(localStorage.getItem("arrayProductos"))
+    let nuevoObjeto = new Producto(categoria, marca, cantidad, precio)
     
     //Logica condicional
     if(localStorage.getItem("arrayProductos") != null){
-        let nuevoObjeto = new Producto(categoria, marca, cantidad, precio)
         paso1.push(nuevoObjeto)
         localStorage.setItem("arrayProductos", JSON.stringify(paso1))
     }else { 
         localStorage.clear()
-        let nuevoObjeto = new Producto(categoria, marca, cantidad, precio)
+        //let nuevoObjeto = new Producto(categoria, marca, cantidad, precio)
         arrayProductos.push(nuevoObjeto)
         localStorage.setItem("arrayProductos", JSON.stringify(arrayProductos))
     }
-    imprimirProductos()
+    imprimirProductos(nuevoObjeto)
 }
 
 //Variables------------------------------------------------------------
 
 
-function imprimirProductos(){
-    let imprimir = JSON.parse(localStorage.getItem("arrayProductos"))
-
-    if(imprimir != null){
-
-   // const mostrarLista = document.getElementById("mostrarLista")
-    imprimir.forEach(e => {
+function imprimirProductos(nuevoObjeto){
 
         let tabla = document.createElement("tr")
 
         let th = document.createElement("th")
 
         th.setAttribute("class", "col-1")
-        th.textContent = `${e.categoria}`
+        th.textContent = nuevoObjeto.categoria
         tabla.appendChild(th)
 
         let th1 = document.createElement("th")
         th1.setAttribute("class", "col-2")
-        th1.textContent = `${e.marca}`
+        th1.textContent = nuevoObjeto.marca
         tabla.appendChild(th1)
 
         let th2 = document.createElement("th")
         th2.setAttribute("class", "col-2")
-        th2.textContent = `${e.cantidad}`
+        th2.textContent = nuevoObjeto.cantidad
         tabla.appendChild(th2)
 
         let th3 = document.createElement("th")
         th3.setAttribute("class", "col-2")
-        th3.textContent = `${e.precio}`
+        th3.textContent = nuevoObjeto.precio
         tabla.appendChild(th3)
 
         lista.appendChild(tabla)
-
-        });
-    }else {
-        console.log("El array estaba nulo");
-    }
+    
 }
 //Eventos--------------------------------------------------------------
 botonGuardar.addEventListener("click", guardarProducto)
