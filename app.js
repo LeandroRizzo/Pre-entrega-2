bb8ad09e396ccca3ddffe2da06559e9e559fb6ab
Leandro Rizzo <86168414+LeandroRizzo@users.noreply.github.com>
2021-07-26 19:54:59 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,80 +1,92 @@
-//Entidades----------------------------------------------------
-class Producto {
-    constructor(categoria, marca, cantidad, precio){
-        this.categoria = categoria,
-        this.marca = marca,
-        this.cantidad = cantidad,
-        this.precio = precio,
-        this.disponible = true
+// Variables
+let products = [];
+
+// Selectores
+let saveButton = $('#guardar');
+let productsContainer = $('#mostrarLista');
+
+// Funciones
+const init = () => {
+    initDatabase();
+
+    products.forEach((product) => {
+        printProduct(product);
+    });
+};
+
+const initDatabase = () => {
+    const lsProducts = localStorage.getItem('products');
+
+    if (lsProducts != null) {
+        try {
+            products = JSON.parse(localStorage.getItem('products'));
+        } catch (error) {
+            initLocalStorage();
+        }
+    } else {
+        initLocalStorage();
     }
-    sumarIva(){
-        return this.precio * 1.21;
-    }
-}
-//Variables-----------------------------------------------------
-//Array
-const arrayProductos = []
+};
 
-//Selectores
-let botonGuardar = document.getElementById("guardar")
-let lista = document.getElementById("mostrarLista")
+const initLocalStorage = () => {
+    localStorage.clear();
+    localStorage.setItem('products', JSON.stringify(products));
+};
 
-//Funciones---------------------------------------------------------
-function guardarProducto(){
-   
-    let categoria = document.getElementById("categoria").value
-    let marca = document.getElementById("marca").value
-    let cantidad = document.getElementById("cantidad").value
-    let precio = document.getElementById("precio").value
+const saveEntity = (product) => {
+    products.push(product);
+    localStorage.setItem('products', JSON.stringify(products));
+};
 
+const saveProduct = () => {
+    const categoria = $('#categoria').val();
+    const marca = $('#marca').val();
+    const cantidad = $('#cantidad').val();
+    const precio = $('#precio').val();
+
+    const product = {
+        categoria,
+        marca,
+        cantidad,
+        precio
+    };
     
+    saveEntity(product);
+    clearForm();
 
-    let paso1 = JSON.parse(localStorage.getItem("arrayProductos"))
-    let nuevoObjeto = new Producto(categoria, marca, cantidad, precio)
-    
-    //Logica condicional
-    if(localStorage.getItem("arrayProductos") != null){
-        paso1.push(nuevoObjeto)
-        localStorage.setItem("arrayProductos", JSON.stringify(paso1))
-    }else { 
-        localStorage.clear()
-        //let nuevoObjeto = new Producto(categoria, marca, cantidad, precio)
-        arrayProductos.push(nuevoObjeto)
-        localStorage.setItem("arrayProductos", JSON.stringify(arrayProductos))
-    }
-    imprimirProductos(nuevoObjeto)
-}
+    printProduct(product);
+};
 
-//Variables------------------------------------------------------------
+const clearForm = () => {
+    $('#nuevoProducto').trigger('reset');
+};
 
+const printProduct = (product) => {
+    let row = $('<tr>');
 
-function imprimirProductos(nuevoObjeto){
+    let categoriaCollumn = $('<th>');
+    categoriaCollumn.text(product.categoria);
+    row.append(categoriaCollumn);
 
-        let tabla = document.createElement("tr")
+    let marcaCollumn = $('<th>');
+    marcaCollumn.text(product.marca);
+    row.append(marcaCollumn);
 
-        let th = document.createElement("th")
+    let cantidadCollumn = $('<th>');
+    cantidadCollumn.text(product.cantidad);
+    row.append(cantidadCollumn);
 
-        th.setAttribute("class", "col-1")
-        th.textContent = nuevoObjeto.categoria
-        tabla.appendChild(th)
+    let precioCollumn = $('<th>');
+    precioCollumn.text(product.precio);
+    row.append(precioCollumn);
 
-        let th1 = document.createElement("th")
-        th1.setAttribute("class", "col-2")
-        th1.textContent = nuevoObjeto.marca
-        tabla.appendChild(th1)
+    productsContainer.append(row);
+};
 
-        let th2 = document.createElement("th")
-        th2.setAttribute("class", "col-2")
-        th2.textContent = nuevoObjeto.cantidad
-        tabla.appendChild(th2)
+//Eventos
+saveButton.click((e) => {
+    saveProduct();
+});
 
-        let th3 = document.createElement("th")
-        th3.setAttribute("class", "col-2")
-        th3.textContent = nuevoObjeto.precio
-        tabla.appendChild(th3)
-
-        lista.appendChild(tabla)
-    
-}
-//Eventos--------------------------------------------------------------
-botonGuardar.addEventListener("click", guardarProducto)
+// Inicializar aplicacion
+init();
